c83e5c372da5d4ca4a35ce14a47708035937747d
Masahiro Watanabe <masahiro.watanabe02@gmail.com>
2024-12-09 12:29:25 +0900

    toEqual

--- sum.test.ts
+++ sum.test.ts
@@ -10,3 +10,12 @@ test('2 + 2 = 4',()=>{
   expect(2 + 2).toBe(4);
 })
 
+//toEqual は、オブジェクトまたは配列のすべてのフィールドを再帰的にチェックする
+test('object assignment', () => {
+  const data = { one: 1};
+  data['two'] = 2;
+  expect(data).toEqual({
+    one:1,
+    two:2
+  })
+})
